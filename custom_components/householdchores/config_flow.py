7c1fa3c6fde910d34c89d8f8664194550cd70a77
Anthony Willis <curlybracketmedia@gmail.com>
2025-10-07 19:00:44 +0100

    Update

--- custom_components/householdchores/config_flow.py
+++ custom_components/householdchores/config_flow.py
@@ -1,26 +1,61 @@
-from datetime import datetime, timedelta, timezone
-from .const import STATUSES
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
+from .const import DOMAIN, CONF_NAME, CONF_LAST_DONE, CONF_NEXT_DUE, CONF_DAYS, CONF_POINTS
 
-def parse_datetime(dt_str):
-    if not dt_str:
-        return None
-    try:
-        return datetime.fromisoformat(dt_str)
-    except Exception:
-        return None
 
-def calculate_status(next_due):
-    if not next_due:
-        return STATUSES["DO_NOT_DO"]
+class HouseholdChoresConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Household Chores."""
 
-    now = datetime.now(timezone.utc)
-    delta = next_due - now
+    VERSION = 1
 
-    if delta.total_seconds() < -172800:  # > 48h past
-        return STATUSES["OVERDUE"]
-    elif delta.total_seconds() < 0:
-        return STATUSES["DUE"]
-    elif delta.total_seconds() < 86400:  # within 24h
-        return STATUSES["DUE_SOON"]
-    else:
-        return STATUSES["NOT_DUE"]
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
+
+        if user_input is not None:
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_NAME): str,
+                vol.Optional(CONF_LAST_DONE): str,
+                vol.Optional(CONF_NEXT_DUE): str,
+                vol.Optional(CONF_DAYS, default=7): int,
+                vol.Optional(CONF_POINTS, default=1): int,
+            }
+        )
+
+        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return HouseholdChoresOptionsFlowHandler(config_entry)
+
+
+class HouseholdChoresOptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle options for an existing chore."""
+
+    def __init__(self, config_entry):
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        """Manage options for the integration."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        data = {**self.config_entry.data, **self.config_entry.options}
+
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_NAME, default=data.get(CONF_NAME)): str,
+                vol.Optional(CONF_LAST_DONE, default=data.get(CONF_LAST_DONE, "")): str,
+                vol.Optional(CONF_NEXT_DUE, default=data.get(CONF_NEXT_DUE, "")): str,
+                vol.Optional(CONF_DAYS, default=data.get(CONF_DAYS, 7)): int,
+                vol.Optional(CONF_POINTS, default=data.get(CONF_POINTS, 1)): int,
+            }
+        )
+
+        return self.async_show_form(step_id="init", data_schema=data_schema)
